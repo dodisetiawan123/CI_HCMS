67a0efb12f97288ef7e122249abaf8e44c79886e
dodisetiawan <dodisetiawan.ti9.poliwangi@gmail.com>
2022-11-28 09:26:11 +0700

    SQL

--- sql/hcms_db.sql
+++ sql/hcms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 03:03 AM
+-- Generation Time: Nov 28, 2022 at 03:23 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
